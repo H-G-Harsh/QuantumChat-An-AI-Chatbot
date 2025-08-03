f8a6f539d2d37fe99d2387c81e82240edd98c9fb
H-G-Harsh <hgharsh495@gmail.com>
2025-08-04 00:02:38 +0530

    wraping clerk4

--- frontend/src/layouts/rootlayout/rootlayout.jsx
+++ frontend/src/layouts/rootlayout/rootlayout.jsx
@@ -1,38 +1,30 @@
-import {Link,Outlet} from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import './rootlayout.css';
-import { ClerkProvider } from '@clerk/clerk-react';
-import { SignedIn,UserButton } from "@clerk/clerk-react";
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-import {useQuery,useMutation,useQueryClient, QueryClient,QueryClientProvider} from '@tanstack/react-query'
+import { SignedIn, UserButton } from "@clerk/clerk-react";
+import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
-
-const queryClient = new QueryClient()
-
+const queryClient = new QueryClient();
 
 const Rootlayout = () => {
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <QueryClientProvider client={queryClient}>
-    <div className='rootlayout'>
-    <header>
-      <Link to="/" className="logo">
-        <img src="/final.jpg" alt=""/>
-        <span>QuantumChat</span>
-      </Link>
-      <div className="user">
-      <SignedIn>
-        <UserButton />
-      </SignedIn></div>
-    </header>
-    <main>
-      <Outlet/>
-    </main>
-    </div>
+      <div className='rootlayout'>
+        <header>
+          <Link to="/" className="logo">
+            <img src="/final.jpg" alt=""/>
+            <span>QuantumChat</span>
+          </Link>
+          <div className="user">
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </header>
+        <main>
+          <Outlet/>
+        </main>
+      </div>
     </QueryClientProvider>
-    </ClerkProvider>
   );
 };
 
