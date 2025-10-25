98adef17edb158e720d229b2a1ff5d92736a54c2
H G Harsh <hgharsh495@gmail.com>
2025-10-25 21:16:12 +0530

    Update Gemini model version to 2.5-flash

--- frontend/src/lib/gemini.js
+++ frontend/src/lib/gemini.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI,HarmCategory,HarmBlockThreshold } from "@google/gene
   
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_PUBLIC_KEY);
 const model = genAI.getGenerativeModel({ 
-    model: "gemini-1.5-flash" 
+    model: "gemini-2.5-flash" 
 });
 
 export default model
